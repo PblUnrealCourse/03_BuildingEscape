ee8a397c021b838c517c313fd3df04c55d39ca76
Pablo Corsino <pbl.corsino@gmail.com>
2017-02-07 07:13:38 +0100

    Method for release action

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -44,6 +44,7 @@ void UGrabber::BeginPlay()
 
 		/// Bind the input axis
 		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
 	}
 	else
 	{
@@ -56,6 +57,12 @@ void UGrabber::BeginPlay()
 void UGrabber::Grab()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+}
+
+// Call when grab is released
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Released"));
 }
 
 // Called every frame
